5582c61f6d474b10cb8a08a90e88669b531e234c
waghmareamar <126074486+waghmareamar@users.noreply.github.com>
2023-03-30 11:13:57 +0530

    Add files via upload

---
+++ Htmlstructure.java
@@ -0,0 +1,14 @@
+package Basic;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Htmlstructure {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get());
+		// TODO Auto-generated method stub
+
+	}
+
+}
